d48c0bd8f6d504add7de2fcb0648ace811c644cd
TASL-00159\Sadriddin <s.sirojiddinov@elsevier.com>
2023-11-22 17:35:44 +0500

    RRN is read according to terminal type

--- src/main/java/uz/agrobank/terminal/service/impl/Cron.java
+++ src/main/java/uz/agrobank/terminal/service/impl/Cron.java
@@ -1,6 +1,5 @@
 package uz.agrobank.terminal.service.impl;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import uz.agrobank.terminal.config.Constants;
@@ -10,14 +9,11 @@ import uz.agrobank.terminal.service.TerminalService;
 public class Cron {
     public final TerminalService terminalService;
 
-    @Value("${terminal.zone}")
-    private String checkoutFilePath;
-
     public Cron(TerminalService terminalService) {
         this.terminalService = terminalService;
     }
 
-    @Scheduled(cron = "0 0 9,17 * * ?", zone = Constants.ZONE)
+    @Scheduled(cron = "0 0 9,16 * * ?", zone = Constants.ZONE)
     public void triggerProductsUpdate() {
         terminalService.sendDailyDetails();
     }
